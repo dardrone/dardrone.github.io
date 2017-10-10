73d41597b4ad0739844313e02f694ed8f64132d0
Darren Jennings <dmjenn02@gmail.com>
2017-10-09 17:34:33 -0700

    Updates

---
+++ path---index-eb452b82ae43cba3ae43.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{"./node_modules/json-loader/index.js!./.cache/json/index.json":function(e,t){e.exports={data:{site:{siteMetadata:{title:"Darren Jennings"}},allMarkdownRemark:{edges:[{node:{excerpt:"tl;dr  Check out  this cool app I built! I recently visited my hometown of Louisville, KY, where the coffee flows like wild horses. I had…",frontmatter:{path:"/spaceman-spiffy/",date:"06 October, 2017",title:"spaceman-spiffy"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-eb452b82ae43cba3ae43.js.map
